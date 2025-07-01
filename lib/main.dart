7bdbc5d6513c2d7aca09ba0a2c6566194b46aa81
Fresnel AKAN <fresnelakan@gmail.com>
2025-07-01 03:05:51 +0100

    ENregistrement

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,14 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+// Si tu utilises les fonctions
+// import 'package:cloud_functions/cloud_functions.dart';
 
 import 'firebase_options.dart';
 import 'wrapper.dart';
+
+
+const String firebaseEmulatorHost = '10.0.2.2'; // Ton IP local
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -14,15 +19,12 @@ void main() async {
   // Utilisez les émulateurs en mode debug
 if (kDebugMode) {
   try {
-
-    FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080); // Le port par défaut de l'émulateur Firestore est 8080
-    // Si vous utilisez l'émulateur Authentication, décommentez la ligne suivante
-    FirebaseAuth.instance.useAuthEmulator('localhost', 9099); // Le port par défaut est 9099
-    // Bien que votre fonction soit déclenchée par Firestore, si vous avez besoin d'appeler des fonctions HTTP/Callable depuis Flutter,
-    // vous devriez configurer l'émulateur Functions ici :
-    // FirebaseFunctions.instance.useFunctionsEmulator('localhost', 5001); // Le port par défaut est 5001
+    // MODIFIE CES LIGNES POUR UTILISER firebaseEmulatorHost
+    FirebaseFirestore.instance.useFirestoreEmulator(firebaseEmulatorHost, 8080); // Le port par défaut de l'émulateur Firestore est 8080
+    FirebaseAuth.instance.useAuthEmulator(firebaseEmulatorHost, 9099); // Le port par défaut est 9099
+    // Si vous utilisez l'émulateur Functions, décommentez et utilisez également firebaseEmulatorHost
+    // FirebaseFunctions.instance.useFunctionsEmulator(firebaseEmulatorHost, 5001); // Le port par défaut est 5001
   } catch (e) {
-    // Gérer les erreurs si les émulateurs ne sont pas en cours d'exécution (facultatif)
     print('Erreur lors de la connexion aux émulateurs : $e');
   }
 }
@@ -40,15 +42,15 @@ class MyApp extends StatelessWidget {
       title: 'Mon App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: const Color(0xFFF5F5F5), // Ou Colors.white
-        colorScheme: ColorScheme.light(
+        scaffoldBackgroundColor: const Color(0xFFF5F5F5),
+        colorScheme: const ColorScheme.light(
           // Couleur de fond globale
         ),
         bottomNavigationBarTheme: BottomNavigationBarThemeData(
           backgroundColor: Colors.white,
           selectedItemColor: Colors.black,
           unselectedItemColor: Colors.grey[700],
-          selectedIconTheme: IconThemeData(color: Colors.black),
+          selectedIconTheme: const IconThemeData(color: Colors.black),
           unselectedIconTheme: IconThemeData(color: Colors.grey[700]),
         ),
       ),
